b2c2946696b7aa8604e3881f1d5f49728142b320
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2025-05-21 16:37:50 +0600

    plant data successfully uploaded to db

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gpe39fa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@plantsserver.ofsyqsf.mongodb.net/?retryWrites=true&w=majority&appName=PlantsServer`
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -25,32 +25,32 @@ async function run() {
     const database = client.db("usersdb");
     const usersCollection = database.collection("users");
 
-    app.get('/users',async(req,res)=>{
+    app.get('/my-plants',async(req,res)=>{
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
-    app.get('/users/:id', async(req,res)=>{
-        const id =req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await usersCollection.findOne(query);
-        res.send(result);
-    })
+    // app.get('/users/:id', async(req,res)=>{
+    //     const id =req.params.id;
+    //     const query = {_id: new ObjectId(id)}
+    //     const result = await usersCollection.findOne(query);
+    //     res.send(result);
+    // })
 
-    app.post("/users", async (req, res) => {
+    app.post("/my-plants", async (req, res) => {
       console.log("data in the server ", req.body);
       const newUser = req.body;
       const result = await usersCollection.insertOne(newUser);
       res.send(result);
     });
 
-    app.delete('/users/:id', async(req,res)=>{
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await usersCollection.deleteOne(query);
-      res.send(result);
-    })
+    // app.delete('/users/:id', async(req,res)=>{
+    //   const id = req.params.id
+    //   const query = {_id: new ObjectId(id)}
+    //   const result = await usersCollection.deleteOne(query);
+    //   res.send(result);
+    // })
 
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -63,9 +63,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("coffee server is active now...!");
+  res.send("Plants server is active now...!");
 });
 
 app.listen(port, () => {
-  console.log(`coffee server is running on port ${port}`);
+  console.log(`Plants server is running on port ${port}`);
 });
